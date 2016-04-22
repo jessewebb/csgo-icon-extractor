ee5720f1cee33bbbab1341aeeccbf57dcf1a4130
Jesse Webb <gweebz@gmail.com>
2016-04-21 18:49:52 -0600

    Add csgo_icon_extractor.parse_output() function

--- csgo_icon_extractor.py
+++ csgo_icon_extractor.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 """ CS:GO Icon Extractor """
 
+import os
 import re
 
 
@@ -27,6 +28,8 @@ class ObjectSetDetails(object):
 
 
 def parse_output_line(output_line):
+    if 'ID(s)' not in output_line:
+        return None
     (info, ids_string) = output_line.split(': ID(s) ')
     info_re = re.compile(r'^ \[(?P<flag>-\w+)\] (?P<count>\d+) (?P<object_type>\w+?)s?$')
     info_match = info_re.match(info)
@@ -36,3 +39,9 @@ def parse_output_line(output_line):
     result.count = int(info_match.group('count'))
     result.ids = parse_ids(ids_string)
     return result
+
+
+def parse_output(output):
+    output_lines = output.split(os.linesep)
+    parsed_lines = [parse_output_line(line) for line in output_lines]
+    return [line_details for line_details in parsed_lines if line_details is not None]
--- csgo_icon_extractor_test.py
+++ csgo_icon_extractor_test.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 """ CS:GO Icon Extractor Tests """
 
-from csgo_icon_extractor import parse_ids, parse_output_line
+from csgo_icon_extractor import parse_ids, parse_output_line, parse_output
 
+import os
 import unittest
 
 
@@ -47,3 +48,22 @@ class ParseOutputLineTests(unittest.TestCase):
         self.assertEqual('JPEG', result.object_type)
         self.assertEqual(12, result.count)
         self.assertEqual([4, 18, 19, 20, 21, 22, 23, 695, 696, 703, 704, 705], result.ids)
+
+    def test_header_line(self):
+        output_line = 'Objects in file iconlib.swf:'
+        result = parse_output_line(output_line)
+        self.assertIsNone(result)
+
+
+class ParseOutputTests(unittest.TestCase):
+    """ Tests for csgo_icon_extractor.parse_output_line() """
+
+    def test_simple_output(self):
+        output = 'Objects in file iconlib.swf:{}'.format(os.linesep) +\
+                 ' [-i] 1 Shape: ID(s) 3{}'.format(os.linesep)
+        result = parse_output(output)
+        self.assertEqual(1, len(result))
+        self.assertEqual('-i', result[0].flag)
+        self.assertEqual('Shape', result[0].object_type)
+        self.assertEqual(1, result[0].count)
+        self.assertEqual([3], result[0].ids)
